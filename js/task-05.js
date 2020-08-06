d60e70881c6538352131e4de8519e8a71d68b65b
Alex-naumov <56648843+Alex-naumov@users.noreply.github.com>
2020-08-06 12:31:20 +0300

    Исправил ошибки

--- js/task-05.js
+++ js/task-05.js
@@ -59,6 +59,7 @@ class Car {
    */
   turnOff() {
     this.isOn = false;
+    this.speed = 0;
   }
 
   /*
@@ -67,8 +68,8 @@ class Car {
    * не больше чем значение свойства maxSpeed
    */
   accelerate(value) {
-    if (value < this.maxSpeed) {
-      this.speed = value;
+    if (this.speed + value <= this.maxSpeed) {
+      this.speed = this.speed + value;
     }
   }
 
@@ -77,7 +78,7 @@ class Car {
    * при условии что результирующая скорость не меньше нуля
    */
   decelerate(value) {
-    if (this.speed - value > 0) {
+    if (this.speed - value >= 0) {
       this.speed = this.speed - value;
     }
   }
@@ -87,7 +88,7 @@ class Car {
    * но только в том случае если машина заведена!
    */
   drive(hours) {
-    if ((this.isOn = true)) {
+    if (this.isOn) {
       this.distance = this.distance + hours * this.speed;
     }
   }
